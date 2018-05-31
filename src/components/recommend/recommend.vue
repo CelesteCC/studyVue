69ceafd9addc37a3dacddd8079598ed5fa7549ab
CelesteCC <418026326@qq.com>
2018-05-31 18:03:54 +0800

    study

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>recommended</div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        name: "recommended"
+    }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
